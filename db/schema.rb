cc84c95ee970a780bf59cc21f440603a3d470551
Chely Ho <adept_inept@yahoo.com>
2018-03-05 19:20:26 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 3) do
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.text "location"
-    t.text "theme"
+    t.string "theme"
     t.float "price"
     t.boolean "family_friendly"
     t.datetime "opening_date"
